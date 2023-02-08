43791f312af85e0423352cd9696522826eecd493
Kailash Gopal D <121489805+thejadelion@users.noreply.github.com>
2023-02-08 14:52:54 +0530

    Add files via upload
    This is for question 3

--- Project_seven/try4.c
+++ Project_seven/try4.c
@@ -1,45 +1,50 @@
 #include <stdio.h>
 #include <math.h>
 
+// defining the function
 float f(float x){
     return tan(M_PI* 50/180)*x - 9.81/(2*625*pow(cos(M_PI*50/180),2))*pow(x,2)+1;
 }
 
 
-float gold(float xl,float xu,float err)
-{   printf("%f %f\n",xl,xu);
-    float er=10;
-    float xopt = 3;
-    float r=(pow(5,0.5)-1)/2;
-
-while (er>err){
-    float p = (xu-xl);
+float gold(float xl,float xu,float err)//defining function that uses golden ratio search
+{   
+    float er=10;//initial value of error to start the loop
+    float xopt;//this the value at which function gives maxima
+    while (2>1){
+    float r=(pow(5,0.5)-1)/2;//defining the value of gold ratio
+    float p = (xu-xl);//the difference between lower and upper limits
     float d = r*p;
-    float x1=xl+d;
+    float x1=xl+d;//defining x1 and x2
     float x2=xu-d;
-    if (f(x1)<f(x2))
-    {
-        xu=x1;
-        printf("22222222222222222");
-        xopt=x2;
-        printf("%f %f %f \n",xl,xu,xopt);
-    }
-    else 
-    {   
-        xl=x2;
-        printf("33333333333333333333");
-        xopt=x1;
-        printf("%f %f %f \n",xl,xu,xopt);
-    }
-    printf("%f %f %f \n",xl,xu,xopt);
-    float er=(1-r)*fabs((xu-xl)/xopt)*100;
-    if (er<err){break;}}
 
-    return (xopt);
+
+
+    if (f(x1)>f(x2))//redefining x1 and x2 if f(x1)>f(x2)
+    {
+        xl=x2;
+        
+        xopt=x1;
+        
+    }
+
+    else //redefining x1 and x2 if f(x1)<f(x2)
+    {  
+        xu=x1;
+        xopt=x2;
+    }
+   
+    float er=(1-r)*fabs((xu-xl)/xopt)*100;//defining the relative error
+    if (er<err){
+        break;
+    }
 }
+    return (xopt);//returning the x at which maxima occurs
+}
+
     
 
 int main()
 {
-    printf("%f",gold(0,60,1));
+    printf("%f, and the value is %f",gold(0,60,0.1), f(gold(0,60,0.1)));//calling the function
 }
